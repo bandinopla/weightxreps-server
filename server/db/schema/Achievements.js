e60f74eae9c4ed8a570789583d45ee2070c8bc21
bandinopla <71508858+bandinopla@users.noreply.github.com>
2024-09-20 16:42:55 +0000

    attempt at trying to start documenting the graphql...

--- server/db/schema/Achievements.js
+++ server/db/schema/Achievements.js
@@ -2,22 +2,48 @@ import gql from "graphql-tag";
 
 const $types = gql` 
 
+  """
+  A generic thing that was achieved. 
+  """
   type Achievement {
     id:ID!
     name:String!
     description:String!
   }
 
+  """
+  Represents the state of an achievement for a particular user. 
+  """
   type AchievementState {
+
+    """
+    ID of the achievement
+    """
     aid:ID!
+
+    """
+    If it was achieved or not
+    """
     gotit:Boolean
     when:YYYYMMDD
+
+    """
+    Details particular to this specific achievement
+    """
     note:String
   }
  
 
   extend type Query {  
+
+    """
+    Returns all the achievements that this user has up to that particular date.
+    """
     getAchievementsStateOf( uid:ID!, asOfThisYMD:YYYYMMDD! ):[AchievementState] 
+
+    """
+    Returns all the available achievements that the system recognizes/has.
+    """
     getAchievements:[Achievement]
   }
 `;
